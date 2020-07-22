5ad7e845a540df98762de3b447c97d8936de22aa
r4tahir <60016890+r4tahir@users.noreply.github.com>
2020-07-22 13:13:31 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function revealMessage() {
+    document.getElementById("hiddenMessage").style.display = 'block';
+}
+
+function revealMessage2() {
+    document.getElementById("hiddenMessage2").style.display = 'block';
+}
+
